5836d17e60cd9cc8cd3c7982bcd20c8b94b500b7
lmn451 <putaluta@tuta.io>
2025-02-20 14:42:39 +0200

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,54 @@
-import './App.css'
+import React, { useState, useCallback } from "react";
+import "./App.css";
 
-export default function App() {
+// const shuffleArray = (array) => array.sort(() => Math.random() - 0.5);
+
+const fruitsList = ["apple", "pineapple", "watermelon", "pear", "grape"];
+
+function App() {
+  const [count, setCount] = useState(1);
+
+  const incrementHandler = useCallback(() => {
+    setCount((prev) => prev + 1);
+  }, [count]);
+
+  const MAP_LABELS = {
+    1: "one",
+    2: "two",
+    3: "three",
+  };
+
   return (
-    <main>
-      React ⚛️ + Vite ⚡ + Replit
-    </main>
-  )
+    <div className="app">
+      <div className="header">
+        <div>
+          <img src="https://storage.googleapis.com/coderpad_project_template_assets/react.svg" />
+          <span>Interview</span>
+        </div>
+      </div>
+      <div className="content">
+        <p>
+          Count: {count} ( {MAP_LABELS[count]} )
+        </p>
+        <button onClick={incrementHandler}>Increment</button>
+        <ComponentOne onClick={() => setCount(1)} />
+      </div>
+      <div className="content">
+        {someStaticArray.map((e) => (
+          <React.Fragment>{e} </React.Fragment>
+        ))}
+      </div>
+    </div>
+  );
 }
+
+function ComponentOne({ onClick }) {
+  return (
+    <React.Fragment>
+      <button onClick={onClick}>Reset</button>
+      {/* Some more JSX */}
+    </React.Fragment>
+  );
+}
+
+export default App;
